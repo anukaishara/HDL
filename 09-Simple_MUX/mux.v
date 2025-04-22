a8ca14b9a6da3c5c6f5a88a06d0aefc5befd8986
Anuka Ishara Dayananda <anukaishara123@gmail.com>
2025-04-22 14:57:36 +0530

    4_22_3

--- 09-Simple_MUX/mux.v
+++ 09-Simple_MUX/mux.v
@@ -3,8 +3,10 @@ module mux_2to1(
     input wire a,
     input wire b,
     input wire se,
+    input wire en, // enable signal
     output wire y
 );
-    assign y = (se) ? a : b;
+    assign y = en ? (se ? a : b) : 1'bz;
+
 
 endmodule
